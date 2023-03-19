832f790814daeca50b4234959789c4f9afe36096
BrunBruno <brunomaslon@gmail.com>
2023-03-19 23:24:51 +0100

    styling learn blocks, creating learn functionality

--- chess/src/components/HomaPage/LearnSection/LearnBlock.jsx
+++ chess/src/components/HomaPage/LearnSection/LearnBlock.jsx
@@ -6,8 +6,21 @@ function LearnBlock(props) {
       className={classes.learn__block}
       onClick={(event) => props.onExpandContent(event, props.index)}
     >
-      <div>
-        <p></p>
+      <div className={classes.outside}>
+        <div className={classes.outside__content}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+          est blanditiis dolores ea omnis corrupti, repellendus libero commodi
+          placeat provident suscipit nihil, fuga nisi, magni asperiores
+          excepturi? Quo, alias excepturi?
+        </div>
+      </div>
+      <div className={classes.inside}>
+        <div className={classes.inside__content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          error! Labore, cupiditate doloremque sint saepe impedit ut blanditiis
+          eaque velit fuga animi, iusto maxime fugit architecto error odit!
+          Facilis, perferendis.
+        </div>
       </div>
     </div>
   );
